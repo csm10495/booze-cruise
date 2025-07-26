ca6b75ea6c8e5725e593013ae136e055140be571
Charles Machalow <csm10495@gmail.com>
2025-07-25 17:06:21 -0700

    Live first, then cache if not live

--- sw.js
+++ sw.js
@@ -31,41 +31,33 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then((response) => {
-        // Cache hit - return response
-        if (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. We must clone it so that we can
-        // consume the stream twice: one for the cache and one for the
-        // network.
-        const fetchRequest = event.request.clone();
+        // IMPORTANT: Clone the response. A response is a stream
+        // and can only be consumed once. We must clone it so that
+        // we can consume the stream twice.
+        const responseToCache = response.clone();
 
-        return fetch(fetchRequest).then(
-          (response) => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+        caches.open(CACHE_NAME)
+          .then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
 
-            // IMPORTANT: Clone the response. A response is a stream
-            // and can only be consumed once. We must clone it so that
-            // we can consume the stream twice.
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
+        return response;
+      })
+      .catch(() => {
+        // If network request fails, try to get it from the cache
+        return caches.match(event.request).then((response) => {
+          if (response) {
             return response;
           }
-        ).catch(() => {
-          // If network request fails, try to serve from cache
-          return caches.match('/index.html'); // Fallback to index.html
+          // Fallback to index.html if the requested resource is not in cache
+          return caches.match('/booze-cruise/index.html');
         });
       })
   );
